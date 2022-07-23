68aafbf18a66d08bd34b5bfe8db6560838f99fa3
baizhe <919041098@qq.com>
2022-07-23 14:14:54 +0800

    创建路由

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,13 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get '/users/:id', to: 'users#show'
-  post '/users/', to: 'users#create'
+  namespace :api do
+    namespace :v1 do
+      resources :validation_codes, only: [:create]
+      resources :session, only: [:create, :destory]
+      resources :me, only: [:show]
+      resources :items
+      resources :tags
+    end
+  end
 end

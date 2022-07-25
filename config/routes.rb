abb5b512e945e663a16304ef75e1f0df77a6ab02
baizhe <919041098@qq.com>
2022-07-25 14:54:26 +0800

    添加根路由

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get '/' to, 'home#index'
+  
   namespace :api do
     namespace :v1 do
       resources :validation_codes, only: [:create]

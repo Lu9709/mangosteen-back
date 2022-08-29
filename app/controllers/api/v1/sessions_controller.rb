1ccb225ed3d79485a1b737a6de11900b188081d4
baizhe <919041098@qq.com>
2022-08-29 19:16:16 +0800

    实现登录接口

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,10 +1,20 @@
 class Api::V1::SessionsController < ApplicationController
   def create
-    # 如果测试环境，code 就永远为 123456
+    # 如果测试环境
     if Rails.env.test?
-      # 直接验证码必须为 123456
-    else
-      #
+      return render status: :unauthorized if params[:code] != '123456'
+    else 
+      canSignin = ValidationCode.exists? email: params[:email], code: params[:code], used_at: nil
+      return render status: :unauthorized unless canSignin
     end
+    user = User.find_by_email params[:email]
+    if user.nil?
+      render status: :not_found, json: {error: '用户不存在'}
+    else 
+      render status: :ok, json: {
+        jwt: 'xxxxxxxx'
+      }
+    end
+    
   end
 end

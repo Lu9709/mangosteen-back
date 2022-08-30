ca5249d48ec7c71dea01a2ef5136a022cab77917
baizhe <919041098@qq.com>
2022-08-30 10:14:26 +0800

    添加 auto_jwt中间件

--- app/controllers/api/v1/mes_controller.rb
+++ app/controllers/api/v1/mes_controller.rb
@@ -1,13 +1,6 @@
 class Api::V1::MesController < ApplicationController
   def show
-    # get jwt from header
-    header = request.headers["Authorization"]
-    jwt = header.split(' ')[1] rescue ''
-    # decode jwt
-    payload = JWT.decode jwt, Rails.application.credentials.hmac_secret, true, { algorithm: 'HS256' } rescue nil
-    return head 400 if payload.nil?
-    # get user_id from payload
-    user_id = payload[0]['user_id'] rescue nil
+    user_id = request.env['current_user_id'] rescue nil
     # get user from user_id
     user = User.find user_id
     return head 404 if user.nil?

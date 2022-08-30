a480dca9de1eeca79e8dbae70900aadd91810a01
baizhe <919041098@qq.com>
2022-08-30 16:06:43 +0800

    重构JWT：user.generate_jwt

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -12,11 +12,7 @@ class Api::V1::SessionsController < ApplicationController
     if user.nil?
       render status: :not_found, json: {error: '用户不存在'}
     else
-      payload = { user_id: user.id }
-      token = JWT.encode payload, Rails.application.credentials.hmac_secret, 'HS256'
-      render status: :ok, json: {
-        jwt: token
-      }
+      render status: :ok, json: { jwt: user.generate_jwt }
     end
     
   end

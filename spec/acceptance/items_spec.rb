8278365587e257aa741520430b990b1127d49606
baizhe <919041098@qq.com>
2022-08-30 16:24:59 +0800

    重构API文档

--- spec/acceptance/items_spec.rb
+++ spec/acceptance/items_spec.rb
@@ -3,6 +3,8 @@ require 'rspec_api_documentation/dsl'
 
 resource "验证码" do
   get "/api/v1/items" do
+    # 验证方式：使用基础验证， 值为auth
+    authentication :basic, :auth
     parameter :page, '页码'
     parameter :created_after, '创建时间起点（筛选条件）'
     parameter :created_before, '创建时间终点（筛选条件）'
@@ -12,8 +14,10 @@ resource "验证码" do
     end
     let(:created_after) { '2020-10-10' }
     let(:created_before) { '2020-11-11' }
+    let(:current_user) { User.create email: '1@qq.com' }
+    let(:auth) { "Bearer #{current_user.generate_jwt}" }
     example "获取账目" do
-        11.times do Item.create amount: 100, created_at: '2020-10-30' end
+        11.times do Item.create amount: 100, created_at: '2020-10-30',user_id: current_user.id end
         do_request
         expect(status).to eq 200
         json = JSON.parse response_body

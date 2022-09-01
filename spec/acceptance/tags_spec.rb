516757199926827dce007baf683325284acc5149
baizhe <919041098@qq.com>
2022-09-01 20:33:10 +0800

    完成创建标签API｜更新API文档

--- spec/acceptance/tags_spec.rb
+++ spec/acceptance/tags_spec.rb
@@ -2,8 +2,10 @@ require 'rails_helper'
 require 'rspec_api_documentation/dsl'
 
 resource "标签" do
+  authentication :basic, :auth
+  let(:current_user) { User.create email: '1@qq.com' }
+  let(:auth) { "Bearer #{current_user.generate_jwt}" }
   get "/api/v1/tags" do
-    authentication :basic, :auth
     parameter :page, '页码'
     with_options :scope => :resources do
       response_field :id, 'ID'
@@ -12,14 +14,32 @@ resource "标签" do
       response_field :user_id, '用户ID'
       response_field :deleted_at, '删除时间'
     end
-    let(:current_user) { User.create email: '1@qq.com' }
-    let(:auth) { "Bearer #{current_user.generate_jwt}" }
     example "获取标签" do
       11.times do Tag.create name: 'x', sign: 'x', user_id: current_user.id end
       do_request
       expect(status).to eq 200
       json = JSON.parse response_body
       expect(json['resources'].size).to eq 10
+    end
+  end
+  post "/api/v1/tags" do
+    parameter :name, '名称', required: true
+    parameter :sign, '符号', required: true
+    with_options :scope => :resources do
+      response_field :id, 'ID'
+      response_field :name, '名称'
+      response_field :sign, '符号'
+      response_field :user_id, '用户ID'
+      response_field :deleted_at, '删除时间'
+    end
+    let (:name) { 'name' }
+    let (:sign) { 'sign' }
+    example "创建标签" do
+      do_request
+      expect(status).to eq 200
+      json = JSON.parse response_body
+      expect(json['resource']['name']).to eq 'name'
+      expect(json['resource']['sign']).to eq 'sign'
     end
   end
 end

31649fb66514d5e34561be37eddaab9a9e340fc0
baizhe <919041098@qq.com>
2022-09-06 12:20:06 +0800

    后端登录校验

--- spec/requests/api/v1/validation_codes_spec.rb
+++ spec/requests/api/v1/validation_codes_spec.rb
@@ -12,8 +12,7 @@ RSpec.describe "ValidationCodes", type: :request do
       post '/api/v1/validation_codes', params: { email: 'x' }
       expect(response).to have_http_status 422
       json = JSON.parse response.body
-      p json
-      expect(json['errors']['email'][0]).to eq "is invalid"
+      expect(json['errors']['email'][0]).to eq "邮件格式不正确"
     end
   end
 end
